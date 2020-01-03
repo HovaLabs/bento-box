c8ac7dc83252c8123809a111ef2b289798fc532f
matthova <matthova87@gmail.com>
2020-01-03 15:48:50 -0800

    export dimensions context

--- lib/components/index.tsx
+++ lib/components/index.tsx
@@ -1,4 +1,8 @@
 export { Button } from "./Button";
 export { DesignSystemProvider } from "./DesignSystemProvider";
+export {
+  DimensionsContext,
+  DimensionsContextContainer
+} from "./DimensionsContext";
 export { ThemeContext, ThemeContextContainer } from "./ThemeContext";
 export { Text } from "./Text";

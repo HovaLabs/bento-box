4894270ae321db2fe1d8ff08e9cd787e8efeabd6
matthova <matthova87@gmail.com>
2020-02-11 13:13:01 -1000

    removed inline styles rule

--- .eslintrc.js
+++ .eslintrc.js
@@ -25,7 +25,7 @@ module.exports = {
   rules: {
     'react-native/no-unused-styles': 'error',
     'react-native/split-platform-components': 'error',
-    'react-native/no-inline-styles': 'error',
+    // 'react-native/no-inline-styles': 'error',
     'react-native/no-color-literals': 'error',
     'react-native/no-single-element-style-arrays': 'error',
     'react-native/no-raw-text': 'error',
